8ac5bade945d27d085625ea3523aac3289caf796
仲科 万 <64389815@qq.com>
2023-08-17 19:05:55 +0800

    新增sqlite3 knexjs

--- src/dataBase/knexfile.js
+++ src/dataBase/knexfile.js
@@ -8,15 +8,15 @@ const k = knex({
     useNullAsDefault: true
 })
 
-k.schema.createTable('test_chat_records', table => {
-    table.increments('id').primary()
-    table.string('user', 50).notNullable()
-    table.text('message').notNullable()
-    table.timestamps(true, true)
-})
-.then(() => {
-    console.log('Chat records table created');
-})
-.catch(error => {
-    console.error('Error creating chat records table:', error);
-});
+// k.schema.createTable('test_chat_records', table => {
+//     table.increments('id').primary()
+//     table.string('user', 50).notNullable()
+//     table.text('message').notNullable()
+//     table.timestamps(true, true)
+// })
+// .then(() => {
+//     console.log('Chat records table created')
+// })
+// .catch(error => {
+//     console.error('Error creating chat records table:', error)
+// })
