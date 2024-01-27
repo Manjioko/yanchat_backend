a06c80cbceeb72374d13d8823584b311b46374a1
manjioko <643898155@qq.com>
2024-01-27 23:47:15 +0800

    将聊天记录存入数据库后，再返回pong

--- src/code/ws_message.js
+++ src/code/ws_message.js
@@ -50,14 +50,18 @@ async function message(ws, params, data) {
         unread: true,
     }
 
-    //  客户不在线的情况下,也应该响应发送方的信息
-    const pongData = {
-        table_id: chat.to_table,
-        chat_id: chat.chat_id,
-        receivedType: 'pong'
-    }
-    wsClients[chat.user_id]?.send(JSON.stringify(pongData))
     insert(chat.to_table, insertData)
+    .then(res => {
+        // console.log('插入数据库返回值 -> ', res)
+        //  客户不在线的情况下,也应该响应发送方的信息
+        const pongData = {
+            table_id: chat.to_table,
+            chat_id: chat.chat_id,
+            receivedType: 'pong',
+            dbId: res[0]
+        }
+        wsClients[chat.user_id]?.send(JSON.stringify(pongData))
+    })
 }
 
 
